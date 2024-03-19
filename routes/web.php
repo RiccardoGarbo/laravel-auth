1a0ba4a82e490211dad14688d9dd4e726ba24577
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-19 17:25:08 +0100

    add route for admin.project

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', GuestHomeController::class)->name('guest.home');
 
-Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
+Route::prefix('/admin')->name('admin.')->Route::middleware('auth')->group(function () {
+
+    //Rotte Admin Home
+    Route::get('/', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
+    //Rotte Project
+    Route::resource('project', ProjectController::class);
+    
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
