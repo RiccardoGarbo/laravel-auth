3b1f215e390a4a0caf3c535310bd60de9bd19d33
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-20 15:22:42 +0100

    improve admin/index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -9,29 +9,46 @@
     <h1 class="text-center">Projects</h1>
 </header>
 <main class="container">
-<div class="row">
-     @foreach ($projects as $project )       
-     <div class="col-6">
-         <div class="card">
-             <img src="{{$project->image}}" class="{{$project->title}}" alt="...">
-             <div class="card-body">
-                 <h5 class="card-title">{{$project->title}}</h5>
-                 <p class="card-text">{{$project->content}}</p>
-                 <div class="d-flex justify-content-between">
-                    <a href="{{route('admin.projects.show' , $project)}}" class="btn btn-primary">Details</a>
-                    <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <input class="btn btn-danger" type="submit" value="Delete">
-                    </form>
-                 </div>
-                
-                </div>
-            </div>
-        </div>
+
+    <table class="table ">
+        <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Image</th>
+                <th scope="col">Created at</th>
+                <th scope="col">Updated at</th>
+            </tr>
+        </thead>
+        @foreach ($projects as $project )       
+        <tbody>
+          <tr>
+            <th scope="row">{{$project->title}}</th>
+            <td>{{$project->content}}</td>
+            <td>{{$project->image}}</td>
+            <td>{{$project->created_at}}</td>
+            <td>{{$project->updated_at}}</td>
+            <td>
+            <div class="d-flex gap-2"> 
+              <a href="{{route('admin.projects.show' , $project)}}" class="btn btn-primary">Details</a>
+              <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
+              @csrf
+              @method('PATCH')
+              <input class="btn btn-warning" type="submit" value="Edit">
+              </form>
+              <form action="{{route('admin.projects.destroy', $project)}}" method="POST">
+                  @csrf
+                  @method('DELETE')
+                  <input class="btn btn-danger" type="submit" value="Delete">
+                  </form>
+              </div>
+            </td>
+         </tr>
+        </tbody>
         @endforeach
+      </table>
         
-</div>
+
 </main>
 
 @endsection
